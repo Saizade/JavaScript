0a031e818d81d82b757110e6f9932cd5dba910d3
Saizade <sai.zade01@gmail.com>
2025-09-05 10:14:02 +0000

    changes done

---
+++ 01_Basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 2005
+let accountEmail = "sai.zade01@gmail.com"
+let accountPassword = "sai12345"
+accountCity = "Jalgaon"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+// const cannot be changed 
+// we can use variables without defining but it is not a good practice as here we write accountCity
+
+accountEmail = "saizade@gmail.com"
+accountPassword = "zade12345"
+accountCity = "Wardha"
+let accountState
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
