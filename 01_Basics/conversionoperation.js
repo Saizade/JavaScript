14f54423da9e0d10d4d03dac5b63f7f222657c3d
Saizade <sai.zade01@gmail.com>
2025-09-05 13:14:58 +0000

    changes done

---
+++ 01_Basics/conversionoperation.js
@@ -0,0 +1,19 @@
+let score = "33"
+console.log(typeof score)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+
+// "33"=> converted to number 33
+// "33abc"=> gives output as NaN if we converted to number
+// true=> 1, false=> 0
+let isLoggedIn = 1
+let booleanloggedIn = Boolean(isLoggedIn)
+console.log(booleanloggedIn)
+
+// likewise we can convert the number into boolen, string , number.
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
