c84c77bc9259c810371ad0104641342ae5d918e2
Saizade <sai.zade01@gmail.com>
2025-09-05 13:49:00 +0000

    changes done

---
+++ 01_Basics/comparision.js
@@ -0,0 +1,14 @@
+console.log(2 > 1)
+console.log(2 < 1)
+console.log(2 >= 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+console.log("2" > 1)
+console.log("02" > 1)
+// it automatically converts the string value to number 
+
+
+
+// === this is strict check means it do not converts any data type automatically it checks the data type also 
+console.log("2" === 2)
