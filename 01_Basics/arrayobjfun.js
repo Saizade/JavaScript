6eb544d3197ae57112c61732012fcdc3d36a16e7
Saizade <sai.zade01@gmail.com>
2025-09-05 15:56:35 +0000

    changes done

---
+++ 01_Basics/arrayobjfun.js
@@ -0,0 +1,11 @@
+// array
+const heros = ["Thor","Ironman","Batman"]
+// Object
+let myObj = {
+    name : "Sai",
+    age : 18
+}
+// Function
+const myFunction = function(){
+    console.log("Hello World")
+}
