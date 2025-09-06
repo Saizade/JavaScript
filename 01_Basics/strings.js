201b42a9382e667f4db667df2ba8c78b91e65884
Saizade <sai.zade01@gmail.com>
2025-09-06 11:53:10 +0000

    changes done

---
+++ 01_Basics/strings.js
@@ -0,0 +1,17 @@
+const name = "Sai"
+const repoCount = 5
+console.log(`Hello my name is ${name} and my repo count is ${repoCount} `)
+console.log(name.charAt(1))
+console.log(name.indexOf('S'))
+
+// To remove spaces
+const newName = "      Zade    "
+console.log(newName)
+console.log(newName.trim())
+
+// To replace 
+const url = "https://Sai.com/"
+console.log(url.replace('Sai','Zade'))
+
+// To check whether the string is present or not
+console.log(url.includes('Sai'))
