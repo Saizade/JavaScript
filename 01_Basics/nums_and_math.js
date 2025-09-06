0ba8605be2801f7870c701496b8023d151579651
Saizade <sai.zade01@gmail.com>
2025-09-06 13:26:27 +0000

    changes done

---
+++ 01_Basics/nums_and_math.js
@@ -0,0 +1,14 @@
+// to convert number to string
+const number = 500
+console.log(number.toString());
+
+// to get the estimate value like after the decimal 
+console.log(number.toFixed(2))
+
+// maths
+console.log(Math.min(6,5,4,3))
+console.log(Math.max(6,5,4,3))
+console.log(Math.sqrt(25))
+
+// to get a random value between 0 and 1
+console.log(Math.random());
